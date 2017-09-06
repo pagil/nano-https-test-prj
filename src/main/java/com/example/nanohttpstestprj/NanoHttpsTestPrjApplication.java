a8d89b1ccd17b3193b504c4a8bd16061168f01a8
Viktar Yafimchyk <viktar@unlockd.com>
2017-09-06 16:35:19 +1000

    Initial commit

---
+++ src/main/java/com/example/nanohttpstestprj/NanoHttpsTestPrjApplication.java
@@ -0,0 +1,12 @@
+package com.example.nanohttpstestprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoHttpsTestPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoHttpsTestPrjApplication.class, args);
+	}
+}
